4431c1177f79ec2b16b66746bcbbbf7719a20247
Eddie Fong <fonged@oocl.com>
2018-11-20 17:44:42 +0800

    add end > 10 function

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -19,3 +19,7 @@ it ("2,4 case", () => {
 it ("2,5 case", () => {
 	expect(createMultiplicationTableByRange(2, 5)).toBe("2*2=4  \n2*3=6  3*3=9  \n2*4=8  3*4=12 4*4=16 \n2*5=10 3*5=15 4*5=20 5*5=25 \n");
 });
+
+it ("9,10 case", () => {
+	expect(createMultiplicationTableByRange(8, 10)).toBe("8*8=64   \n8*9=72   9*9=81   \n8*10=80  9*10=90  10*10=100 \n");
+});
--- main.js
+++ main.js
@@ -10,15 +10,15 @@ function createMultiplicationTableByRange (start, end) {
 	if (checkValid(start,end) === false) return "";
 	let result = "";
 	let i,j;
-	let width = (end*end).toString().length + 1;
+	let width = (end*end).toString().length + end.toString().length + 1;
 
 	for (i = start; i <= end; i++) { 
 		for (j = start; j <= i; j++) { 
-			result += j + "*" + i + "=" + j*i + " ".repeat(width - (i*j).toString().length);
+			result += j + "*" + i + "=" + j*i + " ".repeat(width - (i*j).toString().length - i.toString().length);
 		}
 		result += "\n";    
 	}
 	return result;
 }
-
+console.log(createMultiplicationTableByRange(2,4));
 module.exports = createMultiplicationTableByRange;
